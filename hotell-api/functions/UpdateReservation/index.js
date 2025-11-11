252d792c92108d7ffd4c2e9c344bee56007a1b4e
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2025-11-11 11:31:07 +0100

    added error handling

--- hotell-api/functions/UpdateReservation/index.js
+++ hotell-api/functions/UpdateReservation/index.js
@@ -3,23 +3,24 @@ const {
 	DynamoDBDocumentClient,
 	UpdateCommand,
 } = require("@aws-sdk/lib-dynamodb");
-const { sendResponse } = require("../responses/index");
+const { sendResponse } = require("../../responses/index");
 
-const client = new DynamoDBClient();
+const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 exports.handler = async (event) => {
 	try {
 		const updateAttributes = JSON.parse(event.body);
 		const roomId = updateAttributes.roomId;
-		console.log("Room ID: ", roomId);
 
-		//Checka att roomId är med
+		if (!roomId) return sendResponse(400, { message: "Room id not provided." });
+		if (updateAttributes.reservedId == null)
+			return sendResponse(404, { message: "Reservation not found." });
 
 		const updateExpression =
 			"set " +
 			Object.keys(updateAttributes)
-				.map((attribute) => `${attribute} = :${attribute}`)
+				.map((attribute) => `#${attribute} = :${attribute}`)
 				.join(", ");
 
 		const expressionAttributeValues = Object.keys(updateAttributes).reduce(
@@ -30,12 +31,21 @@ exports.handler = async (event) => {
 			{}
 		);
 
+		const expressionAttributeNames = Object.keys(updateAttributes).reduce(
+			(names, attributeName) => {
+				names[`#${attributeName}`] = attributeName;
+				return names;
+			},
+			{}
+		);
+
 		const command = new UpdateCommand({
 			TableName: "room-db",
 			Key: { PK: "hotel", SK: roomId },
 			ReturnValues: "ALL_NEW",
 			UpdateExpression: updateExpression,
 			ExpressionAttributeValues: expressionAttributeValues,
+			ExpressionAttributeNames: expressionAttributeNames,
 		});
 
 		const response = await docClient.send(command);
