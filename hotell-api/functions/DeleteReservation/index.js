f25c2553af10a4f3fb4de118845dd7daf20e71e4
Zeldisen <jeanette.norberg82@gmail.com>
2025-11-11 10:34:39 +0100

    deletefunction

--- hotell-api/functions/DeleteReservation/index.js
+++ hotell-api/functions/DeleteReservation/index.js
@@ -1,8 +1,45 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, UpdateCommand } = require("@aws-sdk/lib-dynamodb");
+const { sendResponse } = require("../../responses/index");
+
+const client = new DynamoDBClient({});
+const db = DynamoDBDocumentClient.from(client);
+
 exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Delete",
-    }),
-  };
+  try {
+    const { id } = event.pathParameters;
+    // const { roomId } = JSON.parse(event.queryStringParameters);
+
+    const params = {
+      TableName: "room-db",
+      Key: {
+        PK: "hotel",
+        SK: `ROOM-${id}`
+      },
+
+      UpdateExpression:
+        "SET reservedId = :n, checkIn = :n, checkOut = :n, guests = :n, #n = :n, email = :n",
+      ExpressionAttributeNames: {
+        "#n": "name"
+      },
+      ExpressionAttributeValues: {
+        ":n": null
+      },
+
+      ReturnValues: "ALL_NEW"
+    };
+
+    const result = await db.send(new UpdateCommand(params));
+
+    return sendResponse(200, {
+      message: "Fields set to NULL",
+      reservation: result.Attributes
+    });
+
+  } catch (err) {
+
+    console.error("Failed to clear reservation", err);
+
+    return sendResponse(400, { error: err.message });
+  }
 };
